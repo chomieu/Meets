1a92255c1414032ef3f22a5da5b1b00389a29190
Daniel Sasser <desasser79@gmail.com>
2021-01-27 15:11:23 -0800

    established backend boilerplate

---
+++ controllers/controller.js
@@ -0,0 +1,9 @@
+var express = require("express");
+
+var router = express.Router();
+
+// use router.get router.post router.put router.delete
+
+
+// Export routes for server.js to use.
+module.exports = router;
