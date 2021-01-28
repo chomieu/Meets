53d2061e4fb242473758e851421d1b334b236682
Tyler Thibault <thibault.tyler@gmail.com>
2021-01-27 19:27:44 -0800

    basic login and reg form outlined

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,7 +3,13 @@ var express = require("express");
 var router = express.Router();
 
 // use router.get router.post router.put router.delete
+router.get("/", (req, res) => {
+    res.render("index")
+})
 
+router.get("/register", (req, res) => {
+    res.render("partials/register");
+})
 
 // Export routes for server.js to use.
 module.exports = router;
