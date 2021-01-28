2c015d70e82c596133bb5ea99bdbb03dd643eab3
chomieu <chomieu@gmail.com>
2021-01-27 22:09:58 -0800

    Moved all ai javascript into server.js

--- ai/server.js
+++ ai/server.js
@@ -42,3 +42,46 @@ const dialogflowFulfillment = (request, response) => {
   agent.handleRequest(handler)
 
 }
+
+var SpeechSDK;
+var recognizer;
+$("document").ready(function () {
+
+  $("#recordBtn").on("click", function () {
+    $("#recordBtn").prop("disabled", true)
+    // Use the subscription key and configure the SpeechSDK object provided by the file referenced in the index.html file.
+    var speechConfig = SpeechSDK.SpeechConfig.fromSubscription(process.env.MSSDK_key, "eastus");
+    // Set speech recognition language to US English
+    speechConfig.speechRecognitionLanguage = "en-US"
+    // Add the user's microphone input
+    var audioConfig = SpeechSDK.AudioConfig.fromDefaultMicrophoneInput();
+    // Create the SpeechRecognizer object
+    recognizer = new SpeechSDK.SpeechRecognizer(speechConfig, audioConfig);
+
+    // Start the voice recognition method. May need to replace with StartContinuousRecognitionAsync later on for more prolonged recognition
+    recognizer.recognizeOnceAsync(
+      // If we're successful.
+      function (result) {
+        // Make the button to start speech recognition work again.
+        $("#recordBtn").prop("disabled", false)
+
+        // Log the result.
+        window.console.log(result)
+
+        // Close the SpeechRecognizer object, and set the variable to undefined.
+        recognizer.close();
+        recognizer = undefined;
+      },
+      // If there's an error.
+      function (err) {
+        // Also sets the button to work again.
+        $("#recordBtn").prop("disabled", false)
+        // log error to the console.
+        window.console.log(err);
+
+        // Close the SpeechRecognizer object, and set the variable to undefined.
+        recognizer.close();
+        recognizer = undefined;
+      });
+  });
+})
