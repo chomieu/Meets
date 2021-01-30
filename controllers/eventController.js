5ef8a0e7efce06bec9888d06688a109969c5a890
Elias Calagiu <eliasc81@gmail.com>
2021-01-30 13:25:34 -0800

    added user login logic to put route

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -51,6 +51,7 @@ const db = require("../models");
     // PUT route to UPDATE events
     router.put("/", function(req, res){
       if(req.session.user){
+        if(req.session.user.id===parseInt(req.params.id)){
       db.Event.update(req.body,
         {
           where: {
@@ -62,6 +63,7 @@ const db = require("../models");
           console.log(err.message);
           res.status(500).send(err.message)
         })
+      }
       } else{
         res.send("please sign in")
       }
