13c765831050a0e2738a9e57695a5532053f31a3
chomieu <chomieu@gmail.com>
2021-01-31 21:15:58 -0800

    Integrated Echo-AI to receive custom audio output

--- routes/ai-routes.js
+++ routes/ai-routes.js
@@ -1,28 +1,42 @@
 const db = require("../models");
-const dialogflow = require('@google-cloud/dialogflow'); 
+const dialogflow = require("@google-cloud/dialogflow").v2;
+const path = require('path')
+const sound = require("sound-play")
 
 module.exports = function (app) {
   app.post('/api/input', (request, response) => {
-    const projectId = 'meets-dnx9'; 
-    const sessionId = '123456'; 
-    const queries = [request.body.input] 
-    const languageCode = 'en'; 
+    const projectId = 'meets-dnx9';
+    const sessionId = '123456';
+    const queries = [request.body.input]
+    const languageCode = 'en';
     const sessionClient = new dialogflow.SessionsClient(); // Instantiates a session client
-
+    const fs = require('fs'); // for writing audio output
+    const util = require('util');
+    
     async function detectIntent(projectId, sessionId, query, contexts, languageCode) {
       // The path to identify the agent that owns the created intent.
       const sessionPath = sessionClient.projectAgentSessionPath(projectId, sessionId);
+
       // The text query request.
       const request = {
         session: sessionPath,
-        queryInput: { text: { text: query, languageCode: languageCode, }, },
+        queryInput: { 
+          text: { 
+            text: query, 
+            languageCode: languageCode, 
+          }, 
+        }, 
       };
-
-      if (contexts && contexts.length > 0) {
-        request.queryParams = { contexts: contexts, };
+      
+      if (projectId === "echo-fmhq") {
+        request.outputAudioConfig = { audioEncoding: 'OUTPUT_AUDIO_ENCODING_LINEAR_16', }
       }
 
-      const responses = await sessionClient.detectIntent(request);
+      if (contexts && contexts.length > 0) {
+        request.queryParams = { contexts: contexts, }
+      }
+
+      const responses = await sessionClient.detectIntent(request)
       return responses[0];
     }
 
@@ -41,31 +55,42 @@ module.exports = function (app) {
 
     async function executeQueries(projectId, sessionId, queries, languageCode) {
       // Keeping the context across queries let's us simulate an ongoing conversation with the bot
-      let context, aiRes, fromDB
+      let context, aiRes, fromDB, echo
       for (const query of queries) {
         try {
-          aiRes = await (await detectIntent(projectId, sessionId, query, context, languageCode)).queryResult;
+          aiRes = await (await detectIntent(projectId, sessionId, query, context, languageCode));
           // fromDB = await getData(aiRes.parameters.person.name, aiRes.intent.displayName)
           // write a function to choose 1 (past/future/now) event from DB or respond that there's no plan
           // function matchIntent(fromDB) {
           //   var eventJSON = fromDB.Event[0]
           // }
-          switch (aiRes.intent.displayName) {
+          switch (aiRes.queryResult.intent.displayName) {
+            case "Echo":
+              util.promisify(fs.writeFile)("ai-audio.wav", aiRes.outputAudio, 'binary');
+              var filepath = path.join(__dirname + "/../ai-audio.wav")
+              sound.play(filepath)
+              response.send(aiRes.queryResult.fulfillmentText)
+              return
+            // case "AgentNameChange":
+            // case "AgentNameGet":
+            // case "AgentNameSave":
             case "Boss":
-              response.send(`${aiRes.fulfillmentText} `) // pass userID (should be same as sessionID) here
+              echo = `${aiRes.queryResult.fulfillmentText} Chomie` // pass userID (should be same as sessionID) here
+              break
             case "Past":
-              response.send(`${aiRes.fulfillmentText} at 10PM $wag`)
+              echo = `${aiRes.queryResult.fulfillmentText} at 10PM $wag`
               break
             case "Now":
-              response.send(`${aiRes.fulfillmentText} would you like to know what's on their schedule?`)
+              echo = `${aiRes.queryResult.fulfillmentText} would you like to know what's on their schedule?`
               break
             case "Future":
-              response.send(`${aiRes.fulfillmentText} would you like to join them?`)
+              echo = `${aiRes.queryResult.fulfillmentText} would you like to join them?`
               break
-            default: response.send(aiRes.fulfillmentText)
+            default: echo = aiRes.queryResult.fulfillmentText
           }
           // Use the context from this response for next queries
-          context = aiRes.outputContexts;
+          context = aiRes.queryResult.outputContexts;
+          executeQueries("echo-fmhq", sessionId, [echo], languageCode);
         } catch (error) {
           console.log(error);
         }
