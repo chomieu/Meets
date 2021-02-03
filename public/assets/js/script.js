11ccaff63f948e837ab7089d2ec9beec2883142c
Tyler <thibault.tyler@gmail.com>
2021-02-03 10:52:48 -0800

    added styling code

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -32,18 +32,22 @@ function switchColor(color) {
 function setColors(color) {
     var r = document.querySelector(":root");
     if (color === "dark") {
-        $(".colorMode").text("Dark Mode");
+        //  DARK MODE
+        $(".colorMode").text("Light Mode");
         r.style.setProperty("--main-2", "#353535");
         r.style.setProperty("--main-1", "#3c6e71");
-        r.style.setProperty("--accent-1", "#d9d9d9");
-        r.style.setProperty("--accent-2", "#284b63");
+        r.style.setProperty("--accent-1", "#284b63");
+        r.style.setProperty("--accent-2", "#d9d9d9");
         r.style.setProperty("--accent-3", "#ffffff");
+        r.style.setProperty("--text", "white");
     } else {
-        $(".colorMode").text("Light Mode");
+        // LIGHT MODE
+        $(".colorMode").text("Dark Mode");
         r.style.setProperty("--main-1", "#1a535c");
         r.style.setProperty("--main-2", "white");
         r.style.setProperty("--accent-1", "#4ecdc4");
         r.style.setProperty("--accent-2", "#ff6b6b");
         r.style.setProperty("--accent-3", "#ffe66d");
+        r.style.setProperty("--text", "black");
     }
 }
