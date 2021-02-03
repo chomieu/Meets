fbef9ee76355533108ed3dc9fc240faf705887de
chomieu <chomieu@gmail.com>
2021-02-02 21:51:53 -0800

    Set up codes for users connection

--- public/assets/js/connection.js
+++ public/assets/js/connection.js
@@ -1,21 +1,19 @@
 $('document').ready(function () {
+  // Initiates when connectBtn is clicked
   $(".connectBtn").on("click", function () {
-    var id = $(this).data("id")
-    var connectionChange = $(this).data("connection")
+    var id = $(this).data("id") // Grabs the other user's id
+    var connectReq = $(this).data("connection") // Boolean
 
-    var connectionStatus = {
-      connected: connectionChange
-    }
-
-    if (connected) {
+    // If user is trying to connect make a PUT request
+    if (connectReq) {
       $.ajax("/connect", {
         type: "PUT",
         data: { associateId: id }
       }).then(() => {
         location.reload();
       });
-    } else {
-      $.ajax("/disconnect" + id, {
+    } else { // Otherwise make a DELETE request
+      $.ajax("/disconnect", {
         type: "DELETE",
         data: { associateId: id }
       }).then(() => {
