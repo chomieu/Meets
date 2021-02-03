8f9edeea3ae1936cc6de55ee493f853328f41719
Tyler <thibault.tyler@gmail.com>
2021-02-03 10:05:29 -0800

    modifing the dark and light mode

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -33,17 +33,17 @@ function setColors(color) {
     var r = document.querySelector(":root");
     if (color === "dark") {
         $(".colorMode").text("Dark Mode");
-        r.style.setProperty("--color-1", "#d8f3dc");
-        r.style.setProperty("--color-2", "#b7e4c7");
-        r.style.setProperty("--color-3", "#40916c");
-        r.style.setProperty("--color-4", "#74c69d");
-        r.style.setProperty("--color-5", "#52b788");
+        r.style.setProperty("--main-2", "#353535");
+        r.style.setProperty("--main-1", "#3c6e71");
+        r.style.setProperty("--accent-1", "#d9d9d9");
+        r.style.setProperty("--accent-2", "#284b63");
+        r.style.setProperty("--accent-3", "#ffffff");
     } else {
         $(".colorMode").text("Light Mode");
-        r.style.setProperty("--color-1", "#264653");
-        r.style.setProperty("--color-2", "#2a9d8f");
-        r.style.setProperty("--color-3", "#e9c46a");
-        r.style.setProperty("--color-4", "#f4a261");
-        r.style.setProperty("--color-5", "#e76f51");
+        r.style.setProperty("--main-1", "#1a535c");
+        r.style.setProperty("--main-2", "white");
+        r.style.setProperty("--accent-1", "#4ecdc4");
+        r.style.setProperty("--accent-2", "#ff6b6b");
+        r.style.setProperty("--accent-3", "#ffe66d");
     }
 }
